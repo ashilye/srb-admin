a243687c24dd57cc111eca6c06d7bf87e3ca6a0c
gaoguanqi <307590625@qq.com>
2021-05-02 20:00:08 +0800

    删除

--- src/views/core/integral-grade/list.vue
+++ src/views/core/integral-grade/list.vue
@@ -6,6 +6,11 @@
       <el-table-column prop="borrowAmount" label="借款额度"/>
       <el-table-column prop="integralStart" label="积分区间开始"/>
       <el-table-column prop="integralEnd" label="积分区间结束"/>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button type="danger" size="mini" icon="el-icon-delete" @click="removeById(scope.row.id)">删除</el-button>
+        </template>
+      </el-table-column>
     </el-table>
   </div>
 </template>
@@ -26,6 +31,31 @@ export default {
     fetchData() {
       integralGradeApi.list().then(response => {
         this.list = response.data.list
+      })
+    },
+    removeById(id) {
+      console.log('id', id)
+      const _that = this
+      // debugger
+      this.$confirm('此操作将永久删除该记录, 是否继续?', '提示', {
+        confirmButtonText: '删除',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        integralGradeApi.removeById(id).then(response => {
+          _that.$message({
+            showClose: true,
+            message: response.message,
+            type: 'success'
+          })
+          // 删除成功后 刷新页面
+          _that.fetchData()
+        })
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        })
       })
     }
   }
